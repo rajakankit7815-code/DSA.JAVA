03b4c54c3800efde146ed94f71dd290036795a7f
Ankit! <rajakankit7815@gmail.com>
2026-06-03 21:02:56 +0530

    Add files via upload

---
+++ String/RemoveChar.java
@@ -0,0 +1,17 @@
+public class RemoveChar {
+    public static void main(String[] args) {
+        // String s="vsgnsdyswnwwnnnnnagsggsg";
+        String s="aaabbbcccdddeeefffgg";
+        String result=" ";
+        result+=s.charAt(0);
+
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)!=s.charAt(i-1)){
+                result+=s.charAt(i);
+
+            }
+
+        }
+        System.out.println(result);
+    }
+}
