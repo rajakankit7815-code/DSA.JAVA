5eb87a6191c28edf8aa9fd05607a39bf6fec0cd1
Ankit! <rajakankit7815@gmail.com>
2026-06-09 21:39:38 +0530

    Add files via upload

---
+++ Recursion/GreatCommDiv.java
@@ -0,0 +1,16 @@
+public class GreatCommDiv {
+
+    static int gcd(int a,int b){
+        if(b==0)return a;
+        return gcd(b,a%b);
+    }
+
+    public static void main(String[] args) {
+        int a=12;
+        int b=18;
+        System.out.println(gcd(a,b));
+    }
+    
+}
+    
+
---
+++ Recursion/NumReverse.java
@@ -0,0 +1,26 @@
+public class NumReverse {
+    
+     static int reverse(int num){
+        int rev=0;
+        if (num==0) {
+            return rev;
+        }
+        rev = rev*10+num%10;
+        return reverse(num/10);
+
+
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int num = 638276;
+        System.out.println(reverse(num));
+
+    }
+    
+}
+    
